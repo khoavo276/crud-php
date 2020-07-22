c0a21fb474f24bdf6b48efe50e6d995778285c8f
Khoa Vo <khoava@bap.jp>
2020-07-22 16:50:53 +0700

    add init

---
+++ database.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `employees` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `address` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `salary` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
